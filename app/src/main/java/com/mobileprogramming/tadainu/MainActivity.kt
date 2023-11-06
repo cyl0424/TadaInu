9229d267647fafabaa72dbf67cbd338eb908f17d
cyl0424 <20102124@seoultech.ac.kr>
2023-11-07 04:01:51 +0900

    theme, font, colors setting

--- app/src/main/java/com/mobileprogramming/tadainu/MainActivity.kt
+++ app/src/main/java/com/mobileprogramming/tadainu/MainActivity.kt
@@ -2,10 +2,16 @@ package com.mobileprogramming.tadainu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mobileprogramming.tadainu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var mBinding: ActivityMainBinding? = null
+    private val binding get() = mBinding!!
     override fun onCreate(savedInstanceState: Bundle?) {
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
     }
 }
