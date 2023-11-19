5b8e75208e0745134b8ffa46c17f54cdb0349729
sehong00 <sehong00@gmail.com>
2023-11-19 22:35:20 +0900

    add PartnersListSubFragment UI

--- app/src/main/java/com/mobileprogramming/tadainu/partnersFeat/PartnersListSubFragment.kt
+++ app/src/main/java/com/mobileprogramming/tadainu/partnersFeat/PartnersListSubFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import com.mobileprogramming.tadainu.R
+import com.mobileprogramming.tadainu.databinding.FragmentMyPetBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,6 +32,13 @@ class PartnersListSubFragment : Fragment() {
             param2 = it.getString(ARG_PARAM2)
         }
     }
+
+    private var _binding: FragmentMyPetBinding? = null
+    private val binding get() = _binding!!
+
+    private val db = FirebaseFirestore.getInstance()
+    private val storage = FirebaseStorage.getInstance()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
