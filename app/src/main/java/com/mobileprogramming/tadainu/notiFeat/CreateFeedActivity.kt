da426162616e891fbeeb7f9a06ae8e8a1e4c10af
JuseungL <juseung0619@gmail.com>
2023-11-23 01:28:28 +0900

    Create Post without design

--- app/src/main/java/com/mobileprogramming/tadainu/notiFeat/CreateFeedActivity.kt
+++ app/src/main/java/com/mobileprogramming/tadainu/notiFeat/CreateFeedActivity.kt
@@ -1,127 +1,163 @@
 package com.mobileprogramming.tadainu.notiFeat
 
 import android.content.Intent
-import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.*
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContentProviderCompat.requireContext
-import androidx.core.content.ContextCompat
-import java.io.File
-import java.text.SimpleDateFormat
-import java.util.*
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
-import com.mobileprogramming.tadainu.R
-import com.mobileprogramming.tadainu.databinding.ActivityCameraPreviewBinding
 import com.mobileprogramming.tadainu.databinding.ActivityCreateFeedBinding
+import com.yalantis.ucrop.UCrop
+import java.io.File
+import java.util.UUID
+
 
 class CreateFeedActivity : AppCompatActivity() {
-
-    private lateinit var cameraExecutor: ExecutorService
-    private lateinit var imageCapture: ImageCapture
     private lateinit var galleryButton: Button
+    private lateinit var imageView: ImageView
     private lateinit var cameraButton: Button
     private lateinit var previewImageTextView: TextView
-    lateinit var binding: ActivityCreateFeedBinding
-
-    private lateinit var photoFile: File
-    private var imageCaptureUri: Uri? = null
+    private lateinit var binding: ActivityCreateFeedBinding
+    private var selectedImageUris: ArrayList<Uri> = ArrayList()
+    private var croppedImageUris: ArrayList<Uri> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_feed)
         binding = ActivityCreateFeedBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        cameraExecutor = Executors.newSingleThreadExecutor()
-        galleryButton = findViewById(R.id.galleryButton)
-        cameraButton = findViewById(R.id.cameraButton)
-        previewImageTextView = findViewById(R.id.previewImage)
+        galleryButton = binding.galleryButton
+        imageView = binding.imageView
+        cameraButton = binding.cameraButton
+        previewImageTextView = binding.previewImage
+        imageView.visibility = View.INVISIBLE
 
-        // Set up the Camera capture
-        imageCapture = ImageCapture.Builder().build()
-
-        // Set up button click listeners
+        // 갤러리 선택 시
         galleryButton.setOnClickListener {
             openGallery()
         }
+        // 카메라 선택 시
         cameraButton.setOnClickListener {
             val intent = Intent(this@CreateFeedActivity, CameraPreviewActivity::class.java)
             startActivity(intent)
         }
     }
 
-
-    private fun takePhoto() {
-        // Create a timestamped file to save the photo
-        val photoFile = createPhotoFile()
-
-        // Set up ImageCapture metadata
-        val metadata = ImageCapture.Metadata().apply {
-            isReversedHorizontal = false
-        }
-
-        // Create output options for ImageCapture
-        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile)
-            .setMetadata(metadata)
-            .build()
-
-        // Capture the image
-        imageCapture.takePicture(
-            outputOptions, ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageSavedCallback {
-                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                    imageCaptureUri = Uri.fromFile(photoFile)
-                    previewImageTextView.text = "Image saved: ${photoFile.absolutePath}"
-                }
-
-                override fun onError(exception: ImageCaptureException) {
-                    Log.e(TAG, "Photo capture failed: ${exception.message}", exception)
-                }
-            }
-        )
-    }
-
-    private fun createPhotoFile(): File {
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
-        val storageDir: File = getExternalFilesDir(null)!!
-        return File.createTempFile("JPEG_${timeStamp}_", ".jpg", storageDir).apply {
-            photoFile = this
-        }
-    }
-
+    // 갤러리 열기
     private fun openGallery() {
-        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        // 갤러리를 여는 Intent
+        val galleryIntent = Intent(Intent.ACTION_GET_CONTENT)
+        galleryIntent.type = "image/*"
+        // 여러개 선택 가능 하도록
+        galleryIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+        // 결과를 처리하기 위해 resultLauncher를 사용하여 갤러리 액티비티 시작
         resultLauncher.launch(galleryIntent)
     }
 
     private val resultLauncher: ActivityResultLauncher<Intent> =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            // RESULT_OK는 -1임
             if (result.resultCode == RESULT_OK) {
                 val data: Intent? = result.data
-                val selectedImageUri: Uri? = data?.data
-                // Handle the selected image URI, e.g., display in ImageView
-                previewImageTextView.text = "Selected image from gallery: $selectedImageUri"
+
+                // 이미지 여러 개일 때 갤러리에서 선택한 이미지들을 처리
+                if (data?.clipData != null) {
+                    val clipData = data.clipData
+
+                    // 선택한 이미지들을 담을 리스트 초기화
+                    selectedImageUris = ArrayList()
+
+                    // 모든 선택된 이미지에 대해 반복
+                    for (i in 0 until clipData!!.itemCount) {
+                        val selectedImageUri = clipData!!.getItemAt(i).uri
+
+                        // 리스트에 선택한 이미지 추가
+                        selectedImageUris.add(selectedImageUri)
+                    }
+
+                    // 선택된 모든 이미지들을 처리하는 함수 호출
+                    processSelectedImages(selectedImageUris)
+                } else if (data?.data != null) {
+                    // 단일 이미지 일때
+                    // 선택한 이미지의 URI를 가져옴
+                    val selectedImageUri = data.data
+
+                    // 리스트를 초기화하고 선택한 이미지를 추가
+                    if (selectedImageUri != null) {
+                        selectedImageUris = arrayListOf(selectedImageUri)
+                        processSelectedImages(selectedImageUris)
+                    }
+                }
             }
         }
+    private fun processSelectedImages(selectedImageUris: ArrayList<Uri>) {
+        // Pass the list of URIs directly to startCropActivity
+        startCropActivity(selectedImageUris, 0)
+    }
+    private var processedImageCount = 0;
+    private fun startCropActivity(sourceUris: ArrayList<Uri>, index: Int) {
+        if (index < sourceUris.size) {
+            Log.d("ITM","startcropactivity$index")
+            val sourceUri = sourceUris[index]
+            val destinationUri = Uri.fromFile(File(cacheDir, "cropped_image_${UUID.randomUUID()}.jpg"))
+            val uCrop = UCrop.of(sourceUri, destinationUri)
+                .withAspectRatio(1f, 1f)
 
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+            val options = UCrop.Options()
+            options.setCompressionFormat(Bitmap.CompressFormat.JPEG)
+            options.setCompressionQuality(80)
+            uCrop.withOptions(options)
+
+            val uCropIntent = uCrop.getIntent(this)
+            cropActivityResultLauncher.launch(uCropIntent)
+        } else {
+            Log.d("ITM","startcropactivity$index")
+            // All images have been processed, start the next activity
+            val intent = Intent(this, DescriptionFeedActivity::class.java)
+            intent.putParcelableArrayListExtra("croppedImageUris", croppedImageUris)
+            Log.d("ITM", "Before Passing Uris : ${croppedImageUris}")
+            startActivity(intent)
+
+            // Reset the counter for future use
+            processedImageCount = 0
+        }
     }
 
+    private val cropActivityResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data: Intent? = result.data
+                if (data != null) {
+                    val resultUri: Uri? = UCrop.getOutput(data)
+                    Log.d("ITM", "resultUri: $resultUri")
+                    if (resultUri != null) {
+                        Log.d("ITM","launcher에서 ")
+                        croppedImageUris.add(resultUri)
+                    }
+
+                    processedImageCount++
+
+                    // Process the next image
+                    startCropActivity(selectedImageUris, processedImageCount)
+                }
+            } else if (result.resultCode == UCrop.RESULT_ERROR) {
+                val error: Throwable? = UCrop.getError(result.data!!)
+                Log.e(TAG, "Error cropping image: ${error?.message}", error)
+            }
+        }
     companion object {
         const val TAG = "CreateFeedActivity"
         const val REQUEST_CODE_PERMISSIONS = 10
-        val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        val REQUIRED_PERMISSIONS = arrayOf(
+            android.Manifest.permission.CAMERA,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        private const val UCROP_REQUEST_CODE = 123
     }
 }
